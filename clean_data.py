907a8d0f55bc88d4c13db63bab9d666a0d78d907
Don Harris <donald.harrison@utah.edu>
2025-01-27 23:35:18 -0500

    adding gitignore for cleaned data file. added some code for where we are sticking code for cleaning the csv data and added example of how its used in beginning of main.ipynb

--- clean_data.py
+++ clean_data.py
@@ -1,5 +1,13 @@
 import pandas as pd
-
+import re
 #
 imbd_df = pd.read_csv('Resources/imdb_all_years.csv')
+
+#Clean Up Title Data
+for i in range(0, imbd_df['Title'].count()):
+    imbd_df.loc[i, 'Title'] = re.sub(r'[0-9]*[.]', '', imbd_df['Title'].iloc[i])
+
+imbd_df['Title'] = imbd_df['Title'].astype('string')
+imbd_df['Movie Link'] = imbd_df['Title'].astype('string')
+
 imbd_df.to_csv('Resources/cleaned_data.csv', index=False)
